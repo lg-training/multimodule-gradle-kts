3d2b80f54e68fbb42e03c22d7030ade9db030c87
Luis García <105936384+lglabs@users.noreply.github.com>
2024-04-15 21:42:06 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ subprojects {
         repositories {
             mavenLocal()
             maven {
-                url = https://maven.pkg.github.com/lg-training/multimodule-gradle-kts
+                url = "https://maven.pkg.github.com/lg-training/multimodule-gradle-kts"
             }
         }
     }
